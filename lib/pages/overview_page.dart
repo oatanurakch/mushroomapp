f105d3087b7f4a2800edaa5020d3a8ca6de8baca
Anurak Choosri <oatty6106164@gmail.com>
2024-07-24 17:50:40 +0700

    Update data

--- lib/pages/overview_page.dart
+++ lib/pages/overview_page.dart
@@ -11,15 +11,23 @@ class OverviewPage extends StatefulWidget {
 class _OverviewPageState extends State<OverviewPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: ColorPalette[0],
-      ),
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height * 0.5,
-        decoration: BoxDecoration(
-          gradient: GreenGradientToneLTToRB,
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: ColorPalette[0],
+              ),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.5,
+                decoration: BoxDecoration(
+                  gradient: GreenGradientToneLTToRB,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
