7ef5242db6d2f4bf9c61c285e0eeb25a3cdd2eb7
Polapong10 <Polapong10@gmail.com>
2024-10-30 12:18:34 +0700

    ten branch

--- lib/pages/utility_page.dart
+++ lib/pages/utility_page.dart
@@ -23,6 +23,7 @@ class _UtilityPageState extends State<UtilityPage> {
   bool _isSwitchOnOffTimeLAMP = false;
   bool _isSwitchOnOffTimeLAMP2 = false;
   bool _isSwitchLAMP = false;
+  bool _isSwitchUV = false;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -75,6 +76,19 @@ class _UtilityPageState extends State<UtilityPage> {
                       ],
                     ),
                   ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            _buildControlUV('UV', 'assets/icons/uv.png')
+                          ],
+                        )
+                      ],
+                    )
+                  )
                 ],
               ),
             ],
@@ -559,6 +573,266 @@ class _UtilityPageState extends State<UtilityPage> {
                           onChanged: (value) {
                             setState(() {
                               _isSwitchLAMP = value;
+                            });
+                          },
+                        ),
+                        Text(
+                          'ON',
+                          style: TextStyle(fontFamily: 'JetBrainsMono'),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.6,
+                height: MediaQuery.of(context).size.height * 0.25,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1,
+                  ),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Auto Mode',
+                      style: TextStyle(fontFamily: 'JetBrainsMono'),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(
+                                8.0,
+                              ), // Add some padding so the border doesn't touch the text
+                              decoration: BoxDecoration(
+                                color: ColorPalette[0],
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(
+                                  color: ColorPalette[
+                                      2], // Set this to the color you want
+                                  width: 2, // Set this to the width you want
+                                ),
+                              ),
+                              child: Text(
+                                "6:30 AM",
+                                style: TextStyle(fontFamily: 'JetBrainsMono'),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(
+                                8.0,
+                              ), // Add some padding so the border doesn't touch the text
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: ColorPalette[0],
+                                border: Border.all(
+                                  color: ColorPalette[
+                                      2], // Set this to the color you want
+                                  width: 2, // Set this to the width you want
+                                ),
+                              ),
+                              child: Text(
+                                "7:30 AM",
+                                style: TextStyle(fontFamily: 'JetBrainsMono'),
+                              ),
+                            ),
+                            Switch(
+                              value: _isSwitchOnOffTimeLAMP,
+                              activeColor: ColorPalette[3],
+                              activeTrackColor: Colors.green[300],
+                              inactiveTrackColor: Colors.white,
+                              inactiveThumbColor: ColorPalette[3],
+                              onChanged: (value) {
+                                setState(() {
+                                  _isSwitchOnOffTimeLAMP = value;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(
+                                8.0,
+                              ), // Add some padding so the border doesn't touch the text
+                              decoration: BoxDecoration(
+                                color: ColorPalette[0],
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(
+                                  color: ColorPalette[
+                                      2], // Set this to the color you want
+                                  width: 2, // Set this to the width you want
+                                ),
+                              ),
+                              child: Text(
+                                "2:30 PM",
+                                style: TextStyle(fontFamily: 'JetBrainsMono'),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(
+                                8.0,
+                              ), // Add some padding so the border doesn't touch the text
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: ColorPalette[0],
+                                border: Border.all(
+                                  color: ColorPalette[
+                                      2], // Set this to the color you want
+                                  width: 2, // Set this to the width you want
+                                ),
+                              ),
+                              child: Text(
+                                "5:30 PM",
+                                style: TextStyle(fontFamily: 'JetBrainsMono'),
+                              ),
+                            ),
+                            Switch(
+                              value: _isSwitchOnOffTimeLAMP2,
+                              activeColor: ColorPalette[3],
+                              activeTrackColor: Colors.green[300],
+                              inactiveTrackColor: Colors.white,
+                              inactiveThumbColor: ColorPalette[3],
+                              onChanged: (value) {
+                                setState(() {
+                                  _isSwitchOnOffTimeLAMP2 = value;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        TimeOfDay? pickedTime = await showTimePicker(
+                          context: context,
+                          initialTime: TimeOfDay.now(),
+                        );
+
+                        if (pickedTime != null) {
+                          print(pickedTime
+                              .format(context)); // Replace this with your code
+                        }
+                      },
+                      child: Text(
+                        'New Schedule',
+                        style: TextStyle(
+                          fontFamily: 'JetBrainsMono',
+                          color: Colors.black,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            WidgetStatePropertyAll(ColorPalette[0]),
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(
+                                color: ColorPalette[2],
+                                width: 2,
+                              )),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+  _buildControlUV(title, pathIcons) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+      width: MediaQuery.of(context).size.width - 20,
+      height: MediaQuery.of(context).size.height * 0.3,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.white,
+      ),
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontFamily: 'JetBrainsMono',
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 0.3,
+                height: MediaQuery.of(context).size.height * 0.25,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1,
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Manual Mode',
+                      style: TextStyle(fontFamily: 'JetBrainsMono'),
+                    ),
+                    Spacer(),
+                    Image.asset(
+                      pathIcons,
+                      height: MediaQuery.of(context).size.height * 0.1,
+                    ),
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'OFF',
+                          style: TextStyle(fontFamily: 'JetBrainsMono'),
+                        ),
+                        Switch(
+                          value: _isSwitchUV,
+                          activeColor: ColorPalette[2],
+                          inactiveThumbColor: ColorPalette[3],
+                          thumbColor: WidgetStatePropertyAll(ColorPalette[3]),
+                          activeTrackColor: Colors.green[300],
+                          onChanged: (value) {
+                            setState(() {
+                              _isSwitchUV = value;
                             });
                           },
                         ),
